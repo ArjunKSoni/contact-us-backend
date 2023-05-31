9ca839c3441cf8193568217b875ad36974d27c1d
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-05-31 18:19:44 +0530

    .

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
+const nodemailer = require("nodemailer");
 const port = process.env.PORT || 4000
 
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: "ArjunKSoni1234@gmail.com",
+        pass: "ejgtvltvjjzgzuzt",
+    },
+});
 
 // session creation   npm i express-session
 // var session=require("express-session")
@@ -13,34 +23,21 @@ app.get("/", (req, res) => {
     res.send("api is live")
 })
 
-app.post("/", (req, res) => {
-    
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: "ArjunKSoni1234@gmail.com",
-        pass: "AKSoni@1234",
-      },
-    });
-    const options = {
+app.post("/", async (req, res) => {
+    let options = {
       from: "ArjunKSoni1234@gmail.com",
       to: 'aksoni0520@gmail.com',
-      subject: "Assignment React Native by Arjun Kumar Soni",
-      message: `Name:- ${req.body.name}
-      Email:- ${req.body.email}
-      Mobile Number:- ${req.body.mobile}
-      Message:- ${req.body.message}`,
+        subject: "Assignment React Native by Arjun Kumar Soni",
+        html: req.body.html
     };
 
     transporter.sendMail(options, (err) => {
       if (err) console.log(err)
       else { console.log("Message send successfully..") }
     });
+    return res.json({ success: "success" })
 
 })
-
 
 
 app.listen(port, () => {
